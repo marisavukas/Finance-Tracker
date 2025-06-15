18592b8cd4b9bf3c8a9a8c4f62e774a2435e9561
marisavukas <mvukas98@gmail.com>
2025-06-15 19:26:02 -0400

    implemented  react fetch for getting transactions

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/transactions"); //by default fetch() sends GET request
+        if (!response.ok) {
+          throw new Error(`HTTP error. Status: ${response.status}`);
+        }
+        const json = await response.json();
+        setTransactions(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTransactions();
+  }, []); //empty dep array ensures this runs only once on mount
+
+  console.log("transactions", transactions);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Transactions</h1>
+      <ul>
+        {transactions.map((t) => (
+          <li key={t.id}>
+            {t.description} - ${t.amount}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
